bbdfd8be61f94cccec27f9878a07ea2383ef4811
Prasanna Moharil <prasannamoharil1@gmail.com>
2026-05-26 04:49:32 +0530

    fix: swagger enable for azure

--- WMS-Solution/WMS.API/Program.cs
+++ WMS-Solution/WMS.API/Program.cs
@@ -145,11 +145,10 @@ builder.Services
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+
+app.UseSwaggerUI();
 
 app.UseGlobalExceptionMiddleware();
 

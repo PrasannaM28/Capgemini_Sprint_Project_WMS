c02a5faa336a4b0f39676baf0b4f05352a1a32e5
Prasanna Moharil <prasannamoharil1@gmail.com>
2026-05-26 06:05:49 +0530

    fix: apiurl corrected http to https

--- WMS-Solution/WMS.Frontend/src/environments/environment.ts
+++ WMS-Solution/WMS.Frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
 
-  apiUrl: 'http://wms-api-prasanna-a8b2g2gzebf2fvae.centralindia-01.azurewebsites.net/api'
+  apiUrl: 'https://wms-api-prasanna-a8b2g2gzebf2fvae.centralindia-01.azurewebsites.net/api'
 };
